2540dc12b00fc6e232fc49e6c664f076aa06643b
Michael Keller <github@ike.ch>
2016-01-21 14:02:23 +1300

    Fixed native messaging.

--- npm_host/server.js
+++ npm_host/server.js
@@ -3,7 +3,7 @@ var nativeMessage = require('chrome-native-messaging');
 process.stdin
     .pipe(new nativeMessage.Input())
     .pipe(new nativeMessage.Transform(function(msg, push, done) {
-        var reply = "Got this" + msg; // Implemented elsewhere by you.
+        var reply = "Got this: " + msg.text; // Implemented elsewhere by you.
         push(reply);                  // Push as many replies as you like.
         done();                       // Call when done pushing replies.
     }))
