4cb99d5f0fa64ad8bcff4dda16fceb881eaddf30
mikeller <github@ike.ch>
2016-01-28 01:16:01 +1300

    Fixed some bugs. New version.

--- build_resources/generate_release_files.sh
+++ build_resources/generate_release_files.sh
@@ -18,20 +18,21 @@ zip -r  ${CHROME_APP_NAME} chrome_app -x \*/node_modules/\*
 # Build release host app install script
 sed -i -E "s/^(\s*\"version\"\s*:\s*\").*(\"\s*,\s*)$/\1${VERSION}\2/" node_host/package.json
 
-cat node_host/installer_template/part_0 > ${INSTALL_SCRIPT_NAME}
+cat build_resources/install_script_template/part_0 > ${INSTALL_SCRIPT_NAME}
 
-sed -i -E "s/^(VERSION=).*$/\1"${VERSION}"/" ${INSTALL_SCRIPT_NAME}
 sed -i -E "s/^(HOST_SCRIPT_NAME=).*$/\1"${HOST_SCRIPT_NAME}"/" ${INSTALL_SCRIPT_NAME}
 sed -i -E "s/^(MANIFEST_NAME=).*$/\1"${MANIFEST_NAME}"/" ${INSTALL_SCRIPT_NAME}
 
 cat node_host/${HOST_SCRIPT_NAME} >>  ${INSTALL_SCRIPT_NAME}
 
-cat node_host/installer_template/part_1 >> ${INSTALL_SCRIPT_NAME}
+cat build_resources/install_script_template/part_1 >> ${INSTALL_SCRIPT_NAME}
 
 cat node_host/package.json >>  ${INSTALL_SCRIPT_NAME}
 
-cat node_host/installer_template/part_2 >> ${INSTALL_SCRIPT_NAME}
+cat build_resources/install_script_template/part_2 >> ${INSTALL_SCRIPT_NAME}
 
 cat node_host/${MANIFEST_NAME} >>  ${INSTALL_SCRIPT_NAME}
 
-cat node_host/installer_template/part_3 >> ${INSTALL_SCRIPT_NAME}
+cat build_resources/install_script_template/part_3 >> ${INSTALL_SCRIPT_NAME}
+
+chmod ug+x ${INSTALL_SCRIPT_NAME}
